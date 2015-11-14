886e72b7b82c6808ffc3df5b738bf4db45a3c90d
Ryan Moore <ryan.moore@greyrocksoft.com>
2015-11-14 14:13:23 -0700

    Self implementation of the proxying is now functional

--- src/proxy-runner/main.go
+++ src/proxy-runner/main.go
@@ -1,23 +1,60 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/go-zoo/bone"
 )
 
-func proxyHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Proxying request")
+type proxyServer struct {
+	Client *http.Client
+}
 
+func writeHeaders(writer http.ResponseWriter, response *http.Response) {
+	for key, value := range response.Header {
+		writer.Header().Set(key, strings.Join(value, ","))
+	}
+}
+
+func (p *proxyServer) ProxyHandler(response http.ResponseWriter, request *http.Request) {
+	log.Printf("Proxying request to: %s\n", request.Host)
+	request.URL.Host = request.Host
+	request.RequestURI = ""
+
+	if request.URL.Scheme == "" {
+		request.URL.Scheme = "http"
+	}
+
+	clientResponse, err := p.Client.Do(request)
+
+	if err != nil {
+		log.Printf("ERROR - %s\n", err.Error())
+		response.WriteHeader(500)
+	} else {
+		// write over the headers then the response, then the body
+		writeHeaders(response, clientResponse)
+		response.WriteHeader(clientResponse.StatusCode)
+
+		io.Copy(response, clientResponse.Body)
+
+		if err != nil {
+			log.Printf("ERROR - %s\n", err.Error())
+		}
+	}
+	request.Body.Close()
+	clientResponse.Body.Close()
 }
 
 func main() {
 	log.Println("Starting test proxy")
 
+	proxyServer := proxyServer{&http.Client{}}
 	mux := bone.New()
 
-	mux.Handle("/*", http.HandlerFunc(proxyHandler))
+	mux.Handle("/*", http.HandlerFunc(proxyServer.ProxyHandler))
 
 	log.Fatal(http.ListenAndServe(":9090", mux))
 }
