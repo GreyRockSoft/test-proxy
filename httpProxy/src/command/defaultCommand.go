16bd1e02fb5e321045bc15eaffce197cea34a81f
GraciesPadre <ShastasPadre1@gmail.com>
2016-09-23 12:50:27 -0600

    After rebasing the ds3_java_sdk code, I do not see the circumstance where the master object list was bypassing the proxy. So, I'm reverting back to what we had before I started re-writing the response body ip address.

--- httpProxy/src/command/defaultCommand.go
+++ httpProxy/src/command/defaultCommand.go
@@ -4,16 +4,12 @@ import (
 	"net/http"
 	"fmt"
 	"strings"
-    "io/ioutil"
-    "encoding/xml"
-    "strconv"
+	"io"
 )
 
 type DefaultCommand struct {
 	RemoteHost string
 }
-
-var proxyInfo ProxyInfo
 
 func (defaultCommand *DefaultCommand) Execute(responseWriter http.ResponseWriter, request *http.Request) (err error, handled bool) {
 	handled = true
@@ -30,58 +26,22 @@ func (defaultCommand *DefaultCommand) Execute(responseWriter http.ResponseWriter
 	response, err = httpClient.Do(request)
 
 	if err != nil {
-		fmt.Println("Error forwarding request to black pearl: ", err)
+		fmt.Print(err)
 		return
 	}
 
 	defer response.Body.Close()
 
-    var responseBody []byte
-    responseBody, err = ioutil.ReadAll(response.Body)
-    if err != nil {
-        fmt.Println("Error reading response body: ", err)
-        return
-    }
-
-    if IsMasterObjectListXml(string(responseBody)) {
-        responseBody = generateResponseBody(responseBody, response)
-    }
-
 	writeHeaders(responseWriter, response)
 	responseWriter.WriteHeader(response.StatusCode)
 
-    responseWriter.Write(responseBody)
+	io.Copy(responseWriter, response.Body)
 
 	return
 }
 
-func writeHeaders(responseWriter http.ResponseWriter, response *http.Response) {
+func writeHeaders(writer http.ResponseWriter, response *http.Response) {
 	for key, value := range response.Header {
-        responseWriter.Header().Set(key, strings.Join(value, ","))
+		writer.Header().Set(key, strings.Join(value, ","))
 	}
-}
-
-func generateResponseBody(originalResponseBody []byte, response *http.Response) []byte {
-    result := originalResponseBody
-
-    masterObjectList := NewMasterObjectList(originalResponseBody)
-
-    if masterObjectList != nil {
-        masterObjectList.SetNodesEndpoint(proxyInfo.IpAddress())
-        masterObjectList.SetNodesHttpPort(proxyInfo.HttpPort())
-
-        var newResponseBody []byte
-
-        newResponseBody, err := xml.Marshal(masterObjectList)
-
-        if err != nil {
-            fmt.Println("Error marshaling new master object list: ", err)
-        } else {
-            response.Header.Set("Content-Length", strconv.Itoa(len(newResponseBody)))
-
-            result = newResponseBody
-        }
-    }
-
-    return result
 }
